a05afc05ec1785790a7bcfdfcc5109e7441e0fa6
Yufei Ye <yeyf13.judy@gmail.com>
2022-02-07 10:54:13 -0800

    get flow

--- scripts/run_raft.sh
+++ scripts/run_raft.sh
@@ -4,7 +4,7 @@ cd ~/Tools/RAFT/
 
 
 # 0.npz : 1--> 0
-python demo.py --path /private/home/yufeiy2/vhoi/data/DTU/scan65/image/ --out /private/home/yufeiy2/vhoi/data/DTU/scan65/FlowBW --save_flow
+python demo.py --path /private/home/yufeiy2/vhoi/data/DTU/scan65/image/ --out /private/home/yufeiy2/vhoi/data/DTU/scan65/FlowFW --save_flow
 
 # 0.npz: 0 --> 1
-python demo.py --path /private/home/yufeiy2/vhoi/data/DTU/scan65/image/ --out /private/home/yufeiy2/vhoi/data/DTU/scan65/FlowFW --reverse --save_flow
+python demo.py --path /private/home/yufeiy2/vhoi/data/DTU/scan65/image/ --out /private/home/yufeiy2/vhoi/data/DTU/scan65/FlowBW --reverse --save_flow
